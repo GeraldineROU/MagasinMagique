955993b1fb64088783d77e3ebbee0c2e4326d0b4
geraldine <geraldine.rouy@le-campus-numerique.fr>
2024-08-28 16:38:27 +0200

    test added

--- src/test/java/com/magasin/MagasinTest.java
+++ src/test/java/com/magasin/MagasinTest.java
@@ -11,7 +11,42 @@ class MagasinTest {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         Magasin app = new Magasin(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals("foo", app.items[0].name);
+        assertEquals(0, app.items[0].quality);
+        assertEquals(0, app.items[0].sellIn);
+    }
+
+
+    @Test
+    void comteTest() {
+        Item[] items = new Item[] { new Item("Comté", 8, 16)};
+        Magasin app = new Magasin(items);
+        app.updateQuality();
+        assertEquals("Comté", app.items[0].name);
+        assertEquals(7, app.items[0].sellIn);
+        assertEquals(16,app.items[0].quality);
+
+    }
+
+    @Test
+    void comtePerimeTest() {
+        Item[] items = new Item[] { new Item("Comté", -2, 16)};
+        Magasin app = new Magasin(items);
+        app.updateQuality();
+        assertEquals("Comté", app.items[0].name);
+        assertEquals(-3, app.items[0].sellIn);
+        assertEquals(18,app.items[0].quality);
+
+    }
+
+    @Test
+    void vipTest() {
+        Item[] items = new Item[] { new Item("Pass VIP Concert", 0, 16)};
+        Magasin app = new Magasin(items);
+        app.updateQuality();
+        assertEquals("Pass VIP Concert", app.items[0].name);
+        assertEquals(0, app.items[0].quality);
+        assertEquals(-1, app.items[0].sellIn);
     }
 
 }
