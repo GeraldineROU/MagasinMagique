57b11dfe2fe82d5e0f3bdc6eda3d6e0e9df21f36
geraldine <geraldine.rouy@le-campus-numerique.fr>
2024-08-29 20:50:47 +0200

    Golden Master Try Hard

--- src/main/java/com/magasin/Magasin.java
+++ src/main/java/com/magasin/Magasin.java
@@ -9,54 +9,109 @@ class Magasin {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Comté")
-                    && !items[i].name.equals("Pass VIP Concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Kryptonite")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
 
-                    if (items[i].name.equals("Pass VIP Concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
+            if (isComte(i)) {
+                if (isPassed(i) && isQualityBelow50(i)) {
+                    qualityPlusOne(items[i]);
+                    sellInDecreasedBy1(items[i]);
+                } else if (isQualityBelow50(i)) {
+                    qualityPlusOne(items[i]);
+                    sellInDecreasedBy1(items[i]);
+                } else if (!isQualityBelow50(i)) {
+                    sellInDecreasedBy1(items[i]);
                 }
             }
 
-            if (!items[i].name.equals("Kryptonite")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Comté")) {
-                    if (!items[i].name.equals("Pass VIP Concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Kryptonite")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
+            if (isStandardItem(i)) {
+                if (items[i].quality > 0 && !isPassed(i)) {
+                    qualityMinusOne(items[i]);
+                    sellInDecreasedBy1(items[i]);
+                } else if (items[i].quality < 1 && !isPassed(i)) {
+                    sellInDecreasedBy1(items[i]);
+                } else if (isPassed(i) && items[i].quality > 0){
+                    qualityMinusTwo(items[i]);
+                    sellInDecreasedBy1(items[i]);
+                } else if (items[i].quality < 1 && isPassed(i)) {
+                    sellInDecreasedBy1(items[i]);
                 }
             }
+
+            if (isPassVipConcert(i)) {
+                if (!isQualityBelow50(i)) {
+                    sellInDecreasedBy1(items[i]);
+                } else if (items[i].sellIn > 10 && isQualityBelow50(i)) {
+                   qualityPlusOne(items[i]);
+                   sellInDecreasedBy1(items[i]);
+                } else if (isVipPassQualityToBeIncreasedBy2(i) && isQualityBelow50(i)) {
+                   qualityPlus2(items[i]);
+                   sellInDecreasedBy1(items[i]);
+               } else if (isVipPassQualityToBeIncreasedBy3(i) && isQualityBelow50(i)) {
+                   qualityPlus3(items[i]);
+                   sellInDecreasedBy1(items[i]);
+               } else if (isPassed(i)) {
+                   items[i].quality = 0;
+                   sellInDecreasedBy1(items[i]);
+               }
+            }
+
         }
     }
+
+
+    private void sellInDecreasedBy1(Item item) {
+        item.sellIn = item.sellIn - 1;
+    }
+
+    private boolean isPassed(int i) {
+        return items[i].sellIn < 1;
+    }
+
+    private boolean isQualityBelow50(int i) {
+        return items[i].quality < 50;
+    }
+
+    private boolean isKryptonite(int i) {
+        return items[i].name.equals("Kryptonite");
+    }
+
+    private boolean isPassVipConcert(int i) {
+        return items[i].name.equals("Pass VIP Concert");
+    }
+
+    private boolean isComte(int i) {
+        return items[i].name.equals("Comté");
+    }
+
+    private void qualityPlusOne(Item item) {
+        item.quality = item.quality + 1;
+    }
+
+    private void qualityPlus2(Item item) {
+        item.quality = item.quality + 2;
+    }
+
+    private void qualityPlus3(Item item) {
+        item.quality = item.quality + 3;
+    }
+
+    private void qualityMinusOne(Item item) {
+        item.quality = item.quality - 1;
+    }
+
+    private void qualityMinusTwo(Item item) {
+        item.quality = item.quality - 2;
+    }
+
+    private boolean isVipPassQualityToBeIncreasedBy2(int i) {
+        return items[i].sellIn < 11 && items[i].sellIn > 5;
+    }
+
+    private boolean isVipPassQualityToBeIncreasedBy3(int i) {
+        return items[i].sellIn < 6 && items[i].sellIn > 0;
+    }
+
+    private boolean isStandardItem(int i) {
+        return !isComte(i) && !isPassVipConcert(i) && !isKryptonite(i);
+    }
+
 }
--- src/test/java/com/magasin/MagasinTest.java
+++ src/test/java/com/magasin/MagasinTest.java
@@ -1,6 +1,9 @@
 package com.magasin;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -12,8 +15,8 @@ class MagasinTest {
         Magasin app = new Magasin(items);
         app.updateQuality();
         assertEquals("foo", app.items[0].name);
+        assertEquals(-1, app.items[0].sellIn);
         assertEquals(0, app.items[0].quality);
-        assertEquals(0, app.items[0].sellIn);
     }
 
 
@@ -24,7 +27,7 @@ class MagasinTest {
         app.updateQuality();
         assertEquals("Comté", app.items[0].name);
         assertEquals(7, app.items[0].sellIn);
-        assertEquals(16,app.items[0].quality);
+        assertEquals(17,app.items[0].quality);
 
     }
 
@@ -35,7 +38,7 @@ class MagasinTest {
         app.updateQuality();
         assertEquals("Comté", app.items[0].name);
         assertEquals(-3, app.items[0].sellIn);
-        assertEquals(18,app.items[0].quality);
+        assertEquals(17,app.items[0].quality);
 
     }
 
@@ -49,4 +52,143 @@ class MagasinTest {
         assertEquals(-1, app.items[0].sellIn);
     }
 
+    //////////// Test paramétrés : permet en une seule fonction de vérifier plusieurs cas /////////////
+
+
+//    @ParameterizedTest
+//    @CsvFileSource(files = "src/test/resources/items.csv")
+//    void csvSourcedTest(String name, Integer sellIn, Integer quality, String expectedName, Integer expectedSellIn, Integer expectedQuality) {
+//
+//        ////initialisation du test
+//        Item[] items = new Item[] {new Item(name, sellIn, quality)};
+//        Magasin shop = new Magasin(items);
+//
+//        ////execution de la fonction à tester
+//        shop.updateQuality();
+//
+//        ////verification
+//        assertEquals(expectedName, shop.items[0].name);
+//        assertEquals(expectedSellIn, shop.items[0].sellIn);
+//        assertEquals(expectedQuality, shop.items[0].quality);
+//    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "Standard Item,10,20,Standard Item,9,19",
+            "Standard Item,0,10,Standard Item,-1,8",
+            "Standard Item,-1,10,Standard Item,-2,8",
+            "Standard Item,5,0,Standard Item,4,0",
+            "Standard Item,5,1,Standard Item,4,0",
+            "Standard Item,5,50,Standard Item,4,49",
+            "Comté,10,20,Comté,9,21",
+            "Comté,0,20,Comté,-1,21",
+            "Comté,-1,20,Comté,-2,21",
+            "Comté,5,50,Comté,4,50",
+            "Comté,5,49,Comté,4,50",
+            "Pass VIP Concert,15,20,Pass VIP Concert,14,21",
+            "Pass VIP Concert,10,20,Pass VIP Concert,9,22",
+            "Pass VIP Concert,5,20,Pass VIP Concert,4,23",
+            "Pass VIP Concert,1,20,Pass VIP Concert,0,23",
+            "Pass VIP Concert,0,20,Pass VIP Concert,-1,0",
+            "Pass VIP Concert,-1,20,Pass VIP Concert,-2,0",
+            "Pass VIP Concert,5,50,Pass VIP Concert,4,50",
+            "Kryptonite,5,80,Kryptonite,5,80",
+            "Kryptonite,-1,80,Kryptonite,-1,80",
+            "Kryptonite,0,80,Kryptonite,0,80",
+            "Standard Item,0,0,Standard Item,-1,0",
+            "Comté,0,50,Comté,-1,50",
+            "Pass VIP Concert,0,0,Pass VIP Concert,-1,0",
+    })
+
+    void csvSourcedInCodeTest(String name, Integer sellIn, Integer quality, String expectedName, Integer expectedSellIn, Integer expectedQuality) {
+
+        ////initialisation du test
+        Item[] items = new Item[] {new Item(name, sellIn, quality)};
+        Magasin shop = new Magasin(items);
+
+        ////execution de la fonction à tester
+        shop.updateQuality();
+
+
+        ////verification
+        assertEquals(expectedName, shop.items[0].name);
+        assertEquals(expectedSellIn, shop.items[0].sellIn);
+        assertEquals(expectedQuality, shop.items[0].quality);
+    }
+
+    @Test
+    void unNouveauJour() {
+        // Initialisation du test
+        Item[] items = new Item[] {
+                new Item("Standard Item", 10, 20),
+                new Item("Standard Item", 0, 10),
+                new Item("Standard Item", -1, 10),
+                new Item("Standard Item", 5, 0),
+                new Item("Standard Item", 5, 1),
+                new Item("Standard Item", 5, 50),
+                new Item("Comté", 10, 20),
+                new Item("Comté", 0, 20),
+                new Item("Comté", -1, 20),
+                new Item("Comté", 5, 50),
+                new Item("Comté", 5, 49),
+                new Item("Pass VIP Concert", 15, 20),
+                new Item("Pass VIP Concert", 10, 20),
+                new Item("Pass VIP Concert", 5, 20),
+                new Item("Pass VIP Concert", 1, 20),
+                new Item("Pass VIP Concert", 0, 20),
+                new Item("Pass VIP Concert", -1, 20),
+                new Item("Pass VIP Concert", 5, 50),
+                new Item("Kryptonite", 5, 80),
+                new Item("Kryptonite", -1, 80),
+                new Item("Kryptonite", 0, 80),
+                new Item("Standard Item", 0, 0),
+                new Item("Comté", 0, 50),
+                new Item("Pass VIP Concert", 0, 0),
+                new Item("Kryptonite", 0, 80)
+        };
+
+        Item[] expectedItems = new Item[] {
+                new Item("Standard Item", 9, 19),
+                new Item("Standard Item", -1, 8),
+                new Item("Standard Item", -2, 8),
+                new Item("Standard Item", 4, 0),
+                new Item("Standard Item", 4, 0),
+                new Item("Standard Item", 4, 49),
+                new Item("Comté", 9, 21),
+                new Item("Comté", -1, 21),
+                new Item("Comté", -2, 21),
+                new Item("Comté", 4, 50),
+                new Item("Comté", 4, 50),
+                new Item("Pass VIP Concert", 14, 21),
+                new Item("Pass VIP Concert", 9, 22),
+                new Item("Pass VIP Concert", 4, 23),
+                new Item("Pass VIP Concert", 0, 23),
+                new Item("Pass VIP Concert", -1, 0),
+                new Item("Pass VIP Concert", -2, 0),
+                new Item("Pass VIP Concert", 4, 50),
+                new Item("Kryptonite", 5, 80),
+                new Item("Kryptonite", -1, 80),
+                new Item("Kryptonite", 0, 80),
+                new Item("Standard Item", -1, 0),
+                new Item("Comté", -1, 50),
+                new Item("Pass VIP Concert", -1, 0),
+                new Item("Kryptonite", 0, 80)
+        };
+
+        Magasin magasin = new Magasin(items);
+
+        // Exécution du code à tester
+        magasin.updateQuality();
+
+        // Validation du résultat
+        for (int i = 0; i<items.length; i++) {
+            assertItemEquals(expectedItems[i], magasin.items[i]);
+        }
+    }
+
+    private void assertItemEquals(Item expectedItems, Item item) {
+        assertEquals(expectedItems.name, item.name);
+        assertEquals(expectedItems.sellIn, item.sellIn);
+        assertEquals(expectedItems.quality, item.quality);
+    }
 }
